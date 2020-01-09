8ee6869acf27326611719df10b0363ebca13a92a
js_yangmingjie <123456>
2020-01-09 17:34:57 +0800

    临时提交

---
+++ src/main/java/com/jushu/video/entity/MovieSearch.java
@@ -0,0 +1,120 @@
+package com.jushu.video.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author chen
+ * @since 2020-01-08
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class MovieSearch implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 演员列表，多个用|隔开
+     */
+    private String actors;
+
+    /**
+     * 国家
+     */
+    private String country;
+
+    /**
+     * 导演，多个用|隔开
+     */
+    private String director;
+
+    /**
+     * 封面图
+     */
+    @TableField("imgUrl")
+    private String imgUrl;
+
+    /**
+     * 是否完结
+     */
+    @TableField("isOver")
+    private Boolean isOver;
+
+    /**
+     * 影片来源
+     */
+    @TableField("movieSource")
+    private String movieSource;
+
+    /**
+     * 影片类型
+     */
+    @TableField("movieType")
+    private String movieType;
+
+    /**
+     * 影片名称
+     */
+    private String name;
+
+    /**
+     * 爬数据时候用的不用管
+     */
+    @TableField("pageId")
+    private String pageId;
+
+    /**
+     * 详情页
+     */
+    @TableField("pageUrl")
+    private String pageUrl;
+
+    /**
+     * 描述
+     */
+    private String reviews;
+
+    /**
+     * 评分
+     */
+    private String score;
+
+    /**
+     * 更新状态描述
+     */
+    @TableField("statusText")
+    private String statusText;
+
+    /**
+     * 标签，多个用|隔开
+     */
+    private String tags;
+
+    /**
+     * 年份
+     */
+    private String year;
+
+    /**
+     * 爬数据时候用的不用管
+     */
+    @TableField("lastRefreshDate")
+    private LocalDate lastRefreshDate;
+
+
+}
